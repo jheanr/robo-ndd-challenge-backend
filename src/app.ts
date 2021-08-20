7cf4123af47a4e5dd9b87ee9b16150c64e8156e5
Jhean Ramos <jheanr@gmail.com>
2021-08-19 22:01:32 -0300

    feat: adding static files support to be fetched by client

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import './shared/container';
 const app = express();
 
 app.use(express.json());
+app.use(express.static('uploads'));
 app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use(routes);
